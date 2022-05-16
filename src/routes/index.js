06cac823a63eb8065a23d4a1da7f88bad96cf513
Thiago Sales Calabria <thiago.sales@apis3.com>
2022-05-16 18:07:46 -0300

    Formulario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const {Router} = require('express');
 const routes = new Router();
 const homeController = require('../controllers/home.controller');
+const formController = require('../controllers/form.controller');
 
 // Página Inicial
 routes.get('/', homeController.list);
+
+// Formulário
+routes.get('/formulario', formController.list);
 
 module.exports = routes;
