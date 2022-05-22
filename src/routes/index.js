171043219df4603e7cb05b0c56b62af1bc3b4700
Thiago Sales Calabria <thiago.sales@apis3.com>
2022-05-22 16:10:57 -0300

    Página produto interna

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,15 @@ const {Router} = require('express');
 const routes = new Router();
 const homeController = require('../controllers/home.controller');
 const formController = require('../controllers/form.controller');
+const internalProductController = require('../controllers/internalProduct.controller');
 
 // Página Inicial
 routes.get('/', homeController.list);
 
 // Formulário
 routes.get('/contato', formController.list);
+
+// Produto Interna
+routes.get('/produto/:slug?', internalProductController.list);
 
 module.exports = routes;
